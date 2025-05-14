9e943208c42b8c5fa7bd235f67a2a9e17a1fde0e
joel-nwamba <joel.elyon.nwamba@gmail.com>
2025-05-13 21:16:25 -0400

    Added images, and all can be toggles

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const profileToggleOne = document.querySelector(".profile1");
+const profileToggleTwo = document.querySelector(".profile2");
+const profileToggleThree = document.querySelector(".profile3");
+const profileToggleFour = document.querySelector(".profile4");
 const btnProfileOne = document.getElementById("toggle1");
+const btnProfileTwo = document.getElementById("toggle2");
+const btnProfileThree = document.getElementById("toggle3");
+const btnProfileFour = document.getElementById("toggle4");
 
 function toggleProfile() {
-    console.log("clickl me")
     if(profileToggleOne.style.display === "none") {
         profileToggleOne.style.display = "block";
     } else {
@@ -10,6 +15,33 @@ function toggleProfile() {
     }
 }
 
-toggleProfile();
+
+function toggleProfileTwo() {
+    if(profileToggleTwo.style.display === "none") {
+        profileToggleTwo.style.display = "block";
+    } else {
+        profileToggleTwo.style.display = "none";
+    }
+}
+
+function toggleProfileThree() {
+    if(profileToggleThree.style.display === "none") {
+        profileToggleThree.style.display = "block";
+    } else {
+        profileToggleThree.style.display = "none";
+    }
+} 
+
+function toggleProfileFour() {
+    if(profileToggleFour.style.display === "none") {
+        profileToggleFour.style.display = "block";
+    } else {
+        profileToggleFour.style.display = "none";
+    }
+}
+
 
 btnProfileOne.addEventListener("click", toggleProfile);
+btnProfileTwo.addEventListener("click", toggleProfileTwo);
+btnProfileThree.addEventListener("click", toggleProfileThree);
+btnProfileFour.addEventListener("click", toggleProfileFour);   
